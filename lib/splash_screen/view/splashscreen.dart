9b153765b15870d494927b5d81db8efaa443b5bf
vyshnavnarayan~ <vyshnavnarayan35@gmail.com>
2023-02-13 15:40:40 +0530

    bloc conflicts cleared and ss

--- lib/splash_screen/view/splashscreen.dart
+++ lib/splash_screen/view/splashscreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:very_good_app/login/view/login_page.dart';
+import 'package:todo/login/view/login_page.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
